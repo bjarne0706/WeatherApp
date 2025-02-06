f5809343a6dbccb954541655602761ea94c341a8
dchantsev <home@Mac-mini-Dan.local>
2025-02-06 20:25:33 +0100

    Initial Commit

---
+++ Weather App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather App
+//
+//  Created by  Dan on 06.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Weather AppTests/Weather_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppTests.swift
+//  Weather AppTests
+//
+//  Created by  Dan on 06.02.2025.
+//
+
+import Testing
+@testable import Weather_App
+
+struct Weather_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
